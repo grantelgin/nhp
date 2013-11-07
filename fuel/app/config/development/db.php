961b856635714eec9db53fdcc29c739913f6913d
grantelgin <grantelgin@gmail.com>
2013-11-06 22:37:28 -0500

    db, timezone, composer

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=grantelg_nhp',
 			'username'   => 'root',
 			'password'   => 'root',
 		),
